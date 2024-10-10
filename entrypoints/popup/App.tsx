73aa9631f877e177c56e5edf5ed8d5c8b2208c47
mohsinali <mohsinali530121@gmail.com>
2024-10-10 12:26:45 +0530

    setup

---
+++ entrypoints/popup/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="font-bold underline  text-5xl ">hello</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./entrypoints/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
